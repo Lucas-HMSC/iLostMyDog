f52450bdcb627fc0a091e2b7466c816190164413
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-07-14 21:03:32 -0300

    Added Google Fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito';
+import AppLoading from 'expo-app-loading';
+
+import { theme } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_400Regular, 
+    Nunito_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>iLostMyDog</Text>
+      <Text style={styles.text}>iLostMyDog</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +31,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 24,
+    fontFamily: theme.fonts.nunito700,
+    color: theme.colors.black2,
   },
 });
---
+++ mobile/src/styles/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    black1: '#4B3F4E',
+    black2: '#5D5360',
+
+    beige1: '#E7C9A5',
+    beige2: '#F5DCB4',
+
+    pink1: '#FF8087',
+    pink2: '#E6646E',
+  },
+  fonts: {
+    nunito400: 'Nunito_400Regular',
+    nunito700: 'Nunito_700Bold'
+  }
+}
