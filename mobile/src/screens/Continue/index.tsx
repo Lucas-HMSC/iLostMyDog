a5b35ef91f78bbd0bdd90c44bb0b49c63d649c3f
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-07-17 19:37:59 -0300

    Continue Screen

--- mobile/src/screens/Continue/index.tsx
+++ mobile/src/screens/Continue/index.tsx
@@ -1,14 +1,56 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+
+import { Button } from '../../components/Button';
 
 import { styles } from './styles';
 
 export function Continue() {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>
-        Tela de Aviso para Continuar sem cadastro
-      </Text>
+      <BorderlessButton
+        style={styles.buttonBack}
+        onPress={handleGoBack}
+      >
+        <Feather 
+          name='arrow-left'
+          size={20}
+          color='#4B3F4E'
+        />
+      </BorderlessButton>
+
+      <View>
+        <Text style={styles.emoji}>
+          ✋
+        </Text>
+        <Text style={styles.title}>
+          Atenção!
+        </Text>
+      </View>
+
+      <View style={styles.contentContainer}>
+        <Text style={styles.contentText}>
+          Você pode continuar sem cadastro, porém aconselhamos criar sua conta.
+        </Text>
+        <Text style={[styles.contentText, { marginTop: 20 }]}>
+          É gratuito e rápido!
+        </Text>
+      </View>
+
+      <View style={styles.button}>
+        <Button
+          title='Continuar'
+        />
+      </View>
     </View>
   );
 }
