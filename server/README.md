02b351b8b5e07af6b2e7f72d2781fd788a132247
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-09-30 21:51:22 -0300

    README.md

---
+++ server/README.md
@@ -0,0 +1,20 @@
+# Classificação de Raças de Cães com TensorFlow
+### Atualmente o projeto classifica as seguintes raças:
+- Rottweiler
+- Bulldog
+- Pug
+- Pastor Alemão
+- Labrador
+
+### Para executar a aplicação, rode o seguinte comando:
+```python
+python3 classifier.py --image_path imagem.png
+```
+
+### Para imprimir o resultado em um arquivo externo:
+```python
+python3 classifier.py --image_path imagem.png > result.txt
+```
+
+Obs: Bibliotecas necessárias para a execução do projeto estão no arquivo **requirements.txt**
+
--- server/classifier.py
+++ server/classifier.py
@@ -1,10 +1,9 @@
 import tensorflow as tf
 import sys
-import numpy as np
 
 tf.compat.v1.disable_v2_behavior()
 
-# Global variables
+# Varíaveis Globais
 tf.compat.v1.flags.DEFINE_string("image_path", "./images/image1.jpg", "Image to be classified")
 label_path = './retrained_labels.txt'
 model_path = './retrained_graph.pb'
@@ -24,13 +23,18 @@ with tf.compat.v1.gfile.FastGFile(model_path, 'rb') as f:
 with tf.compat.v1.Session() as sess:
     softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
     
-    # predict
+    # Realiza a predição com base no modelo
     predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image})
     
-    # Sort to show labels of first prediction in order of confidence
+    # Ordena os labels para colocar no topo a raça com maior porcentual de semelhança
     top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-    
-    for node_id in top_k:
-        human_string = labels[node_id]
-        score = predictions[0][node_id]
-        print('%s (score = %.5f)' % (human_string, score))
+
+    # Imprime todas as porcentagens de todas as 5 raças
+    # for node_id in top_k:
+    #   human_string = labels[node_id]
+    #   score = predictions[0][node_id]
+    #   print('%s (score = %.5f)' % (human_string, score))
+
+    # Imprime somente o nome da raça
+    human_string = labels[top_k[0]]
+    print(human_string)
