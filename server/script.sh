2fb08e19c2bafff09dc327ce77d79aab5ab2743a
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-10-02 10:45:17 -0300

    Update script.sh

--- server/script.sh
+++ server/script.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+#Script para criação de arquivo JSON com o nome da raça identificada
+
 source ./venv/bin/activate
 
-python3 classifier.py --image_path ../images/teste4.jpg > result.json
+print_in_json() {
+	python3 classifier.py --image_path $1 > result.json
+}
+
+print_in_json $1
